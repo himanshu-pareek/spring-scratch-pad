1fead822abcfe972b2c06544470dddc044e52a9d
Himanshu Pareek <himanshupareekiit01@gmail.com>
2024-04-27 13:22:21 +0000

    type conversion and parser configuration

--- spring-spel/src/test/java/dev/javarush/spring/SpringSPELTest.java
+++ spring-spel/src/test/java/dev/javarush/spring/SpringSPELTest.java
@@ -1,15 +1,21 @@
 package dev.javarush.spring;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.expression.EvaluationContext;
 import org.springframework.expression.Expression;
 import org.springframework.expression.ExpressionParser;
+import org.springframework.expression.spel.SpelParserConfiguration;
 import org.springframework.expression.spel.standard.SpelExpressionParser;
+import org.springframework.expression.spel.support.SimpleEvaluationContext;
 
 public class SpringSPELTest {
 
@@ -72,5 +78,47 @@ public class SpringSPELTest {
         Expression assertExpression = parser.parseExpression("name == 'Nikola Tesla'");
         boolean result = assertExpression.getValue(tesla, Boolean.class);
         assertTrue(result);
+    }
+
+    @Test
+    public void testTypeConversion() {
+        record Simple (List<Boolean> booleanList) {}
+
+        Simple simple = new Simple(new ArrayList<>());
+        simple.booleanList.add(true);
+
+        EvaluationContext evaluationContext = SimpleEvaluationContext.forReadOnlyDataBinding().build();
+
+        ExpressionParser parser = new SpelExpressionParser();
+        parser.parseExpression("booleanList[0]")
+            .setValue(evaluationContext, simple, "false");
+
+        assertFalse(simple.booleanList.get(0));
+
+    }
+
+    @Test
+    public void testParserConfigurationAuthGrowList() {
+        record Demo (List<String> list) {}
+
+        Demo demo = new Demo(new ArrayList<>());
+
+        // Turn on:
+        // - auto null reference initialization
+        // - auto collection growing
+        SpelParserConfiguration configuration = new SpelParserConfiguration(true, true);
+        ExpressionParser parser = new SpelExpressionParser(configuration);
+
+        Expression expression = parser.parseExpression("list[3]");
+        String item = expression.getValue(demo, String.class);
+
+        assertEquals("", item);
+        assertEquals(4, demo.list.size());
+
+        Expression anotherExpression = parser.parseExpression("list[9]");
+        anotherExpression.setValue(SimpleEvaluationContext.forReadOnlyDataBinding().build(), demo, "My String");
+
+        assertEquals(10, demo.list.size());
+        assertEquals("My String", demo.list.get(9));
     }
 }
